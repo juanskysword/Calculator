94a4e80cde5d436abfb23a8883cd61641f3b23a3
Juan Tavera <juanskysword@gmail.com>
2022-01-04 13:11:17 -0800

    final code

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log ("hello world")
+console.log ("What is Ur Favorite Food?")
+// console.log ("hello world")
+const myFavFood = ("Pizza");
+
+let myAge = 25;
+
+console.log (`My favorite food is ${myFavFood} and I am ${myAge}`)
+
+const num1 = 5;
+const num2 = 2;
+
+console.log(num1 + num2)
